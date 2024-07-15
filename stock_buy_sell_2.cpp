c8476920e195a5559a154e8ce2f2d8b5e50752e8
sankalp_iiitr <106346800+indianguy123@users.noreply.github.com>
2024-07-15 23:53:00 +0530

    Create stock_buy_sell_2.cpp

---
+++ stock_buy_sell_2.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int solve(vector<int> &prices,int ind,int buy,vector<vector<int>> &dp){
+        if(ind==prices.size()) return 0;
+        if(dp[ind][buy]!=-1) return dp[ind][buy];
+        int profit=0;
+        if(buy){
+            profit=max(-prices[ind]+solve(prices,ind+1,0,dp),solve(prices,ind+1,1,dp));
+        }
+        else{
+            profit=max(prices[ind]+solve(prices,ind+1,1,dp),solve(prices,ind+1,0,dp));
+        }
+        return dp[ind][buy]=profit;
+    }
+    int maxProfit(vector<int>& prices) {
+        int n=prices.size();
+        vector<vector<int>> dp(n,vector<int>(2,-1));
+        return solve(prices,0,1,dp);
+    }
+};
