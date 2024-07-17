cbea81271a85b4177f1f2cb30fc27d61ffecf1e8
sankalp_iiitr <106346800+indianguy123@users.noreply.github.com>
2024-07-17 10:32:02 +0530

    Create remove_duplicates_in_array.cpp

---
+++ remove_duplicates_in_array.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    vector<int> findDuplicates(vector<int>& nums) {
+        vector<int> ans;
+        for(int i=0;i<nums.size();i++){
+            int ind=abs(nums[i]);
+            if(nums[ind-1]<0) ans.push_back(abs(nums[i]));
+            else nums[ind-1]*=-1;
+        }
+        return ans;
+    }
+};
